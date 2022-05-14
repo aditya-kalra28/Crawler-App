a958925d7d7b037bd21209c3033c53aa9914ca17
Aditya <aditya.kalra28@gmail.com>
2022-05-14 18:57:00 +0530

    init

--- CrawlerApp/settings.py
+++ CrawlerApp/settings.py
@@ -50,11 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'CrawlerApp.urls'
 
